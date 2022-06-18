f7c35fce61700a6d504d1e13eacd322983b6a601
Robert Baxter <roer.baxter@gmail.com>
2022-06-18 19:56:29 -0400

    more cleanup

--- tests/unit/adapters/clinician-test.js
+++ tests/unit/adapters/clinician-test.js
@@ -4,7 +4,6 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Adapter | clinician', function (hooks) {
   setupTest(hooks);
 
-  // Nothing to test here yet
   test('it exists', function (assert) {
     let adapter = this.owner.lookup('adapter:clinician');
     assert.ok(adapter);
--- tests/unit/adapters/office-test.js
+++ tests/unit/adapters/office-test.js
@@ -4,7 +4,6 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Adapter | office', function (hooks) {
   setupTest(hooks);
 
-  // Nothing to test here yet
   test('it exists', function (assert) {
     let adapter = this.owner.lookup('adapter:office');
     assert.ok(adapter);
